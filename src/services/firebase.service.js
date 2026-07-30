fcac5061e32f56b321ebc49ddc7287862569b3cc
Carlos Marquina <carlosscripto@gmail.com>
2026-07-30 23:28:43 +0200

    fix: use environment variables for firebase config

--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage'
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOqI3Iz4DOj8OxDjxgVOyUtlxhIAHHbQQ",
-  authDomain: "minitweet-13068.firebaseapp.com",
-  projectId: "minitweet-13068",
-  storageBucket: "minitweet-13068.firebasestorage.app",
-  messagingSenderId: "639253891557",
-  appId: "1:639253891557:web:f6f60de1833a09e52b870c",
-  measurementId: "G-G2TC0BQT38"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig)

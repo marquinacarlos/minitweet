b1c09928a5444b42f8adccb56c19a3f588ee8ae1
Carlos Marquina <carlosscripto@gmail.com>
2026-07-30 23:16:29 +0200

    feat: update firebase config and add analytics

--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -2,17 +2,20 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { getStorage } from 'firebase/storage'
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCCXbtrx-NDz3LcIRIM6Jj5Q9ewN0pukDE",
-	authDomain: "miniredsocial-83a73.firebaseapp.com",
-	projectId: "miniredsocial-83a73",
-	storageBucket: "miniredsocial-83a73.appspot.com",
-	messagingSenderId: "276706105554",
-	appId: "1:276706105554:web:5bb26e03f9931e7ff6f50b"
+  apiKey: "AIzaSyAOqI3Iz4DOj8OxDjxgVOyUtlxhIAHHbQQ",
+  authDomain: "minitweet-13068.firebaseapp.com",
+  projectId: "minitweet-13068",
+  storageBucket: "minitweet-13068.firebasestorage.app",
+  messagingSenderId: "639253891557",
+  appId: "1:639253891557:web:f6f60de1833a09e52b870c",
+  measurementId: "G-G2TC0BQT38"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
+export const analytics = getAnalytics(app)
 export const db = getFirestore(app)
 export const auth = getAuth(app)
 export const storage = getStorage(app)
